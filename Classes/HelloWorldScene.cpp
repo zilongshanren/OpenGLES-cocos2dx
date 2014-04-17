adad828ecf4c27649f53b5a6c8f9331485ee8e72
andyque <guanghui8827@126.com>
2014-04-17 21:47:57 +0800

    revert some changes

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -41,10 +41,12 @@ void HelloWorld::initialize()
     m_centerPoint = m_screenSize / 2;
     
     vector<ISurface*> surfaces(SurfaceCount);
-    auto path1 = FileUtils::getInstance()->fullPathForFilename("micronapalmv2.obj");
-    auto path2 = FileUtils::getInstance()->fullPathForFilename("Ninja.obj");
-    surfaces[0] = new ObjSurface(path1);
-    surfaces[1] = new ObjSurface(path2);
+//    auto path1 = FileUtils::getInstance()->fullPathForFilename("micronapalmv2.obj");
+//    auto path2 = FileUtils::getInstance()->fullPathForFilename("Ninja.obj");
+//    surfaces[0] = new ObjSurface(path1);
+//    surfaces[1] = new ObjSurface(path2);
+    surfaces[0] = new Cone(3, 1);
+    surfaces[1] = new Sphere(1.4f);
     surfaces[2] = new Torus(1.4, 0.3);
     surfaces[3] = new TrefoilKnot(1.8f);
     surfaces[4] = new KleinBottle(0.2f);
--- Resources/myshader.frag
+++ Resources/myshader.frag
@@ -26,11 +26,11 @@ void main()
     highp float sf = max(0.0, dot(N,H));
     sf = pow(sf, Shininess);
     
-    if (df < 0.1) df = 0.0;
-    else if (df < 0.3) df = 0.3;
-    else if (df < 0.6) df = 0.6;
-    else df = 1.0;
-    sf = step(0.5, sf);
+//    if (df < 0.1) df = 0.0;
+//    else if (df < 0.3) df = 0.3;
+//    else if (df < 0.6) df = 0.6;
+//    else df = 1.0;
+//    sf = step(0.5, sf);
     
     lowp vec3 color = AmbientMaterial + df * Diffuse + sf * SpecularMaterial;
     
